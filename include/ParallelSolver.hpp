b4ee59c38f4b4fd58e6fee03dc3028fe876ad518
Gaia Francesca Riva <gaiafrancesca.riva@mail.polimi.it>
2026-05-30 16:57:22 +0200

    doxygen comments

--- include/ParallelSolver.hpp
+++ include/ParallelSolver.hpp
@@ -5,6 +5,9 @@
 #include <functional>
 #include<string>
 
+/**
+ * @brief ParallelSolver class encapsulates the logic for solving the 2D Poisson equation in parallel using MPI.
+ */
 class ParallelSolver {
 private:
     MpiDomain domain;
@@ -17,14 +20,30 @@ private:
     std::function<double(double, double)> bc;
 
 public:
-    // Constructor takes the MPI domain configuration
+    /**
+     * @brief Constructor for the ParallelSolver class.
+     * * Initializes the solver with the given MPI domain, forcing term, and boundary condition functions
+     */
     ParallelSolver(MpiDomain mpi_dom, std::function<double(double, double)> forcing_term, std::function<double(double, double)> boundary_term);
     
-    // Parallel solve method
+    /**
+     * @brief Solves the 2D Poisson equation in parallel using MPI.
+     * @param max_iter Maximum number of iterations.
+     * @param tol Tolerance for convergence.
+     */
     void solve(int max_iter, double tol);
 
+    /**
+     * @brief Computes the analytical error of the numerical solution.
+     * @param exact_sol The exact solution function.
+     * @return The computed error.
+     */
     double compute_analytical_error(std::function<double(double, double)> exact_sol) const;
 
+    /**
+     * @brief Exports the solution to a VTK file.
+     * @param filename The name of the output file.
+     */
     void export_vtk(const std::string& filename){
         VTKExport exporter(domain, h);
         exporter.export_vtk(U, filename);
